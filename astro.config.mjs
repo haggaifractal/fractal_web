af1b1101e7053b673fe9f8f62cae16fd2754148e
unknown <chagai33@gmail.com>
2026-03-26 16:20:58 +0200

    first1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // הגדרת הדומיין הרשמי לצורך הפקת כתובות SEO (קנוניקל, OG)
-  site: 'https://www.fractal.co.il',
+  // הגדרת הדומיין הרשמי או כתובת השרת האוטומטית
+  site: process.env.URL || 'https://www.fractal.co.il',
   integrations: [tailwind()],
 });
